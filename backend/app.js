846eb60f963d1b62d3386fa84d1bd9cf50a2ca46
Alexandr Bokov <bok.off@gmail.com>
2023-09-08 12:24:19 +0300

    Test commit

---
+++ backend/app.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable import/no-extraneous-dependencies */
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const { login, createUser } = require('./controllers/users');
+const { auth } = require('./middlewares/auth');
+const NotFoundError = require('./errors/notFoundError');
+
+const { PORT = 3000, DB_ADRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+mongoose.connect(DB_ADRESS, {
+  useNewUrlParser: true,
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(3),
+  }).unknown(true),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().pattern(/^\S+@\S+\.\S+$/),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.use(auth);
+
+app.use(cardRouter);
+app.use(userRouter);
+
+// eslint-disable-next-line no-unused-vars
+app.use('*', (req, res) => {
+  throw new NotFoundError('Такой страницы нет');
+});
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
+app.listen(PORT);
